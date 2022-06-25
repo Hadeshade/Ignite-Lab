78629bce6723457693104097aa5452ba5128166f
Marco Nemetala <marcoantonionemetala@gmail.com>
2022-06-25 17:20:32 -0300

    Logica das páginas e dos conteúdos

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import { gql, useQuery } from '@apollo/client'
-import { useEffect, useState } from 'react'
-import { client } from './lib/apollo'
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './lib/apollo';
 import { Event } from './pages/Event';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons{
-      id 
-      title
-    }
-  }
-`
-
-interface Lesson{
-  id: string;
-  title: string;
-}
+import { Router } from './Router';
 
 function App() {
-  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-  console.log(data);
-
   return (
-    <Event></Event>
-  )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
 
-function App(){
-    return(
-        <div> Adicionar Router</div>
-    )
+export function Router(){
+    return (
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/event/lesson/:slug" element={<Event />} />
+      </Routes>
+    );
 }
